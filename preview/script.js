9a116e9b93f219ed0c5ef063ff3f90bfac436f0f
RonOvski1212 <245785gio@gmail.com>
2023-08-04 15:56:48 +0400

    localStorage

--- preview/script.js
+++ preview/script.js
@@ -1,2 +1,27 @@
-'use strict';
+"use strict";
 
+const checkbox = document.querySelector("#checkbox"),
+  form = document.querySelector("form"),
+  change = document.querySelector("#color");
+
+if (localStorage.getItem("isChecked")) {
+  checkbox.checked = true;
+}
+
+if (localStorage.getItem("bg") === "changed") {
+  form.style.backgroundColor = "red";
+}
+
+checkbox.addEventListener("change", () => {
+  localStorage.getItem("isChecked", true);
+});
+
+change.addEventListener("click", () => {
+  if (localStorage.getItem("bg") === "changed") {
+    localStorage.removeItem("bg");
+    form.style.backgroundColor = "blue";
+  } else {
+    localStorage.setItem("bg", "canged");
+    form.style.backgroundColor = "red";
+  }
+});
